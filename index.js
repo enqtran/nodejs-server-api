e403d82a25104e14c3a289f78f35e6c17c199a45
enqtran <enqtran@gmail.com>
2017-08-09 17:21:08 +0700

    up

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ const router = express.Router();
 // middleware to use for all requests
 router.use((req, res, next) => {
     console.log('Connect ...');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     next(); // make sure we go to the next routes and don't stop here
 });
 
